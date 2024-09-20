52281d45d8ed99574dd9246d01d88d3892204c15
declankra <declankramper@gmail.com>
2024-09-20 17:05:18 -0500

    quick updates

--- src/components/LearnMorePage.jsx
+++ src/components/LearnMorePage.jsx
@@ -4,10 +4,33 @@ import { Link } from 'react-router-dom';
 function LearnMorePage() {
   return (
     <div className="learn-more-page">
-      <h1>About Meet Or Not</h1>
+    <h1>Learn More About This Project</h1>
+    
+    <p>
+      This is a fun personal project to:
+      <ol>
+        <li>use generative AI in a web app</li>
+        <li>get more practice building full-stack web apps</li>
+        <li>perhaps offer some utility for people frustrated with unproductive meetings</li>
+      </ol>
+    </p>
+
+    <p>
+      hope you enjoy using this. it was fun to make! i’ll probably update this page sometime 
+      in the future with more resources on how this project came to be.
+    </p>
+
+    <p>
+      for now, I’d appreciate it if you left feedback on the product 
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSdHJbr616yjhPKzp88Kty7hnTVey2ucIcdF8OYE1euaTG3n5g/viewform" target="_blank" rel="noopener noreferrer"> here</a>.
+    </p>
+
+    <div className="credits">
+      <p>Credits: @dkBuilds</p>
       <p>
-        Meet Or Not helps you determine if a meeting is necessary and assists in creating an effective agenda. We aim to eliminate unnecessary meetings and make required meetings more productive.
+        <a href="https://www.declankramper.me" target="_blank" rel="noopener noreferrer">www.declankramper.me</a>
       </p>
+    </div>
       <button>
         <Link to="/">Back to Home</Link>
       </button>
